02f8731b46efd103b31beca2ac204e2febdcc470
army8735 <army8735@qq.com>
2015-05-13 21:38:30 +0800

    config

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 var More = require('more-css');
+var url = require('url');
+var path = require('path');
 
 module.exports = function(source) {
   if(this.cacheable) {
     this.cacheable();
   }
-  return More.parse(source);
+  var config = url.parse(this.query, true).query;
+  if(config.suffix) {
+    More.suffix(config.suffix);
+  }
+  if(config.root) {
+    More.root(config.root);
+  }
+  var more = new More();
+  more.path(this.resource);
+  if(config.configFile) {
+    if(config.configFile.charAt(0) == '/') {
+      more.configFile(config.configFile);
+    }
+    else {
+      more.configFile(path.join(process.cwd(), config.configFile));
+    }
+  }
+  if(config.combo) {
+    return more.parseFile(this.resource, true);
+  }
+  return more.parse(source);
 };
